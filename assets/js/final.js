4a935289868e03cd3212f1f963264fbcdc762da9
LiamH97 <ldharrington5@gmail.com>
2021-12-21 09:34:17 +0000

    added some comments to show what final.js does
    added some comments to show what final.js does

--- assets/js/final.js
+++ assets/js/final.js
@@ -1,3 +1,6 @@
+/**Grabs the recentScore that was stored in localStorage  **/
 const recentScore = localStorage.getItem('recentScore')
+/**Grabs the finalScore element from finished.html which is defaulted to 0 **/
 const finalScore = document.getElementById('finalScore')
+/**Grabs the replaces the inner text of the final score element with the stored score in the recentScore const  **/
 finalScore.innerText = recentScore
